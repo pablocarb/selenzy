ef7ff264f7a05d71cf909f92e292e23a2bb77842
mibsspc2 <pablo.carbonell@manchester.ac.uk>
2017-08-04 11:00:18 +0100

    Some fixes

--- tools/copyrod.sh
+++ tools/copyrod.sh
@@ -3,3 +3,6 @@
 SOURCE=../../selenzy
 TARGET=../../selenzyPro
 rsync -a --delete --exclude='.git/' --exclude='uploads/*' --exclude='notes/' --exclude='tools/' $SOURCE/ $TARGET
+cd ../../
+tar -czvf selenzy.tar.gz selenzyPro
+mv selenzyPro /var/www/html/selenzy
